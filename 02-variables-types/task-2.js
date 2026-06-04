8d421c8c4e0b957549e1472b2d13e9518cddd638
oguzhan-ocakoglu <oguzhanocakoglu@gmail.com>
2026-06-04 17:53:30 +0300

    fixes

--- 02-variables-types/task-2.js
+++ 02-variables-types/task-2.js
@@ -3,3 +3,12 @@
 // ayrı değerler tanımlayın; ardından her bir değişkenin veri türünü typeof ile kontrol edip konsola yazdırın.
 // Referans: ./task-2.md
 
+const name = "Ali";
+let age = 20;
+let isStudent = true;
+const favoriteNumber = 7;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof favoriteNumber);
