840658b5dfbaac7609f030ea2f4187d4dcd08358
oguzhan-ocakoglu <oguzhanocakoglu@gmail.com>
2026-06-04 17:57:43 +0300

    fixes

--- 03-strings/task-3.js
+++ 03-strings/task-3.js
@@ -2,3 +2,14 @@
 // Amaç: Kullanıcı bilgilerini tek bir profil alanında toplama pratiği yapın. Ad ve soyadı birleştirerek tam adı oluşturun,
 // bir string'in uzunluğunu ölçün ve hem ilk hem de son karakterini ekrana yazdırın.
 // Referans: ./task-3.md
+
+const firstName = "Ali";
+const lastName = "Yilmaz";
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+const message = "JavaScript öğreniyorum";
+console.log(message.length);
+console.log(message[0]);
+console.log(message[message.length - 1]);
